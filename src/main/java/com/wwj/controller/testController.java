c5a5005c18a81353aecf9c4e3ee471a42f502701
YuppieJ <1171065420@qq.com>
2019-12-11 16:38:22 +0800

    第三次提交  test

---
+++ src/main/java/com/wwj/controller/testController.java
@@ -0,0 +1,24 @@
+package com.wwj.controller;
+
+import com.wwj.pojo.User;
+import com.wwj.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/test")
+public class testController {
+
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping("/test")
+    @ResponseBody
+    public List<User> queryUserList(){
+        return userService.queryUserList();
+    }
+}
